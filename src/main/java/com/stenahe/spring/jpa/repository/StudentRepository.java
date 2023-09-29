6f89d60ea7e80741b1efed8418762f64a8b42681
Santiago Tena Hernandez <stenahe@gmail.com>
2023-09-29 16:18:38 +0200

    Expand Repository

--- src/main/java/com/stenahe/spring/jpa/repository/StudentRepository.java
+++ src/main/java/com/stenahe/spring/jpa/repository/StudentRepository.java
@@ -4,6 +4,13 @@ import com.stenahe.spring.jpa.entity.Student;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface StudentRepository extends JpaRepository<Student, Long> {
+    public List<Student> findByFirstName(String firstName);
+    public List<Student> findByFirstNameContaining(String name);
+    public List<Student> findByGuardianName(String guardianName);
+    public List<Student> findByLastNameNotNull();
+
 }
